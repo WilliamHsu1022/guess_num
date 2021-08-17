dc941af50f12cfae41dd9b48b6f6504fa7e98ca8
William Hsu <william20041022@gmail.com>
2021-08-17 11:24:50 +0800

    final versio

--- guess_num.py
+++ guess_num.py
@@ -1,6 +1,10 @@
 import random 
+s = input('請決定隨機數字範圍開始值: ')
+e = input('請決定隨機數字範圍結束值: ')
+s = int(s)
+e = int(e)
 
-r = random.randint(1, 100)
+r = random.randint(s, e)
 count = 0
 while True:
 	count = count + 1
@@ -11,7 +15,7 @@ while True:
 		print('這是你猜的第', count, '次')
 		break
 	else:
-	    if i > r :
+	    if i > r : 
 	        print('比答案大')
 	    else:
 	        print('比答案小')
